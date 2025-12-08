d5f0d4a846f29f4508eb912adf8e275f31feafac
Kishan Parmar <kparmar2101@gmail.com>
2025-12-08 15:59:34 +0530

    testcase

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 2025-12-08  Kishan Parmar  <kparmar2101@gmail.com>
 
+	LICENCE : GPL-3.0-or-later
+
+2025-12-08  Kishan Parmar  <kparmar2101@gmail.com>
+
 	Add repo info
 
 
--- testcase/testcase.c
+++ testcase/testcase.c
@@ -1,3 +1,21 @@
+// GCC RTL/GIMPLE Syntax Highlighting Extension
+//
+// Copyright (C) 2025 Kishan Parmar
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
